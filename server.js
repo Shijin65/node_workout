5d33522d08b92e55321a1f7bd77ca4937d273bce
shijin65 <shijincht65@gmail.com>
2023-08-25 23:57:36 +0530

    new commit

--- server.js
+++ server.js
@@ -2,13 +2,12 @@ const http = require("http");
 const { stringify } = require("querystring");
 // require("dotenv").config();
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 
 const server = http.createServer((req,res) => {
-    res.statusCode = 200;
-    res.setHeader("content-type" , "application/json");
-    res.write(JSON.stringify({name : "SHIJIN", age:20,skills:["html","css","javascript","node"]}));
+    res.statusCode = 404;
+    
     res.end();
 });
 server.listen(PORT ,()=>{
