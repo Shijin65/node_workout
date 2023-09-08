a9d62725e60cda37060c433617090b902eee3294
shijin65 <shijincht65@gmail.com>
2023-09-08 23:08:46 +0530

    server

--- server.js
+++ server.js
@@ -1,14 +1,39 @@
 const http = require("http");
 const { stringify } = require("querystring");
-// require("dotenv").config();
+const getReq = require("./methods/get-req");
+const postReq = require("./methods/post-req");
+const putReq = require("./methods/put-req");
+const deleteReq = require("./methods/delete-req");
+const { write } = require("fs");
+let movies = require("./data/MovieData.json");
 
-const PORT = process.env.PORT || 5000;
+
+const PORT = 5000;
 
 
 const server = http.createServer((req,res) => {
-    res.statusCode = 404;
+    req.movie = movies;
+    switch(req.method){
+        case "GET":
+            getReq(req,res);
+            break;
+            case "POST":
+            postReq(req,res);
+            break;
+            case "PUT":
+            putReq(req,res);
+            break;
+            case "DELETE":
+            deleteReq(req,res);
+            break;
+
+            default:
+
+            res.writeHeader(404,{"content-type":"applocation/json"});
+          
+            res.end(JSON.stringify({title:"NOT FOUND",message:"ROUTE NOT FOUND"}));
+    }
     
-    res.end();
 });
 server.listen(PORT ,()=>{
     console.log(`the server is started in port:${PORT}`)
